f733a4fd7774e625f74dc50df987bd8a19ba07ba
lixiong <1772063751@qq.com>
2019-10-24 18:20:07 +0800

    m

--- mj/src/app/pb/Message_Def.lua
+++ mj/src/app/pb/Message_Def.lua
@@ -3,7 +3,7 @@ require("app.pb.Message_pb")
 --消息在此处解析和组合
 Message_Def = Message_Def or {}
 
--- //踢玩家
+-- --踢玩家
 KICK_CLIENT_REASON = {
     CLIENT_REPEAT_LOGIN = 0,
     CLIENT_TOKEN_EXPIRE = 1,
@@ -14,7 +14,7 @@ KICK_CLIENT_REASON = {
     CLIENT_LEAVE_ROOM = 6
 }
 
--- //玩家状态
+-- --玩家状态
 USER_STATE = {
     USER_STATE_INIT = 0,
     USER_STATE_IN_LOBBY = 1,
@@ -26,7 +26,7 @@ USER_STATE = {
     USER_STATE_WAIT_LEAVE_GAME = 7
 }
 
--- //桌子状态
+-- --桌子状态
 ROOM_STATE = {
     ROOM_STATE_INIT = 0,
     ROOM_STATE_WAIT_CREATE = 1,
@@ -39,7 +39,7 @@ ROOM_STATE = {
     ROOM_STATE_GAME_ALL_END = 8
 }
 
--- //操作码
+-- --操作码
 OPERATE_CODE = {
     SUB_OPER_INIT = 0,
     SUB_OPER_CHI = 1,
@@ -50,19 +50,31 @@ OPERATE_CODE = {
     SUB_OPER_HU = 6
 }
 
--- //错误码
-ERRORCODE = {
-    ERROR_INIT = 0, --初始值
-    ERROR_GAME_SERVER_UNUSUAL = -601, --服务器异常
-    ERROR_ROOM_NOT_FIND = -602 --房间没找到
+-- --错误码
+ERRORCODE = 
+{
+	ERROR_INIT											 = 0,								--初始值								
+	ERROR_GAME_SERVER_UNUSUAL							 = -601,							--服务器异常
+	ERROR_ROOM_NOT_FIND									 = -602,							--房间没找到
+	ERROR_TOKEN_NO_EQUAL								 = -603,							--大厅和游戏服务器token不相等
+	ERROR_PLAYER_NOT_EXIT								 = -604,							--玩家不存在
+	ERROR_PLAYER_STATE									 = -605,							--玩家状态错误
+	ERROR_ROOM_NOTCAN_ENTER								 = -606,							--玩家不能进入
+	ERROR_GAME_PLAYER_FULL								 = -607,							--房间满
+	ERROR_GAME_PLAYER_EXIT								 = -608,							--玩家在该房间	
+	ERROR_ROOM_NOT_EXIT									 = -609,							--房间不存在
+	ERROR_SIT_DOWN_FAIL									 = -610,							--玩家坐下失败
+	ERROR_LEAVE_FAILL									 = -6011,							--玩家离开失败
+	ERROR_DISMISS_ROOM_FAILL							 = -6012,							--玩家解散房间失败
+	ERROR_NEED_VOTE_DISMISS_ROOM						 = -6013,							--需要投票解散
 }
 
--- //客户端请求大厅消息
--- 	C2L_PLAYER_LOGIN_SYN			 					= 10001;     					//登入
--- 	C2L_PLAYER_CREATE_ROOM_SYN 							= 10002;						//创建房间
--- 	C2L_PLAYER_ENTER_ROOM_SYN							= 10003;						//加入房间
--- 	C2L_PLAYER_MONEY_UPDATA_SYN							= 10004;						//玩家货币更新
--- //-----------------------------------------------------------------------------------------------------------------------------------
+-- --客户端请求大厅消息
+-- 	C2L_PLAYER_LOGIN_SYN			 					= 10001;     					--登入
+-- 	C2L_PLAYER_CREATE_ROOM_SYN 							= 10002;						--创建房间
+-- 	C2L_PLAYER_ENTER_ROOM_SYN							= 10003;						--加入房间
+-- 	C2L_PLAYER_MONEY_UPDATA_SYN							= 10004;						--玩家货币更新
+-- -------------------------------------------------------------------------------------------------------------------------------------
 
 -- message MSG_C2L_PLAYER_LOGIN_SYN
 -- {
@@ -95,15 +107,15 @@ end
 -- message ROOM_RUlES
 -- {
 -- 	required int32 kindid = 1;
--- 	required int32 paytype = 2;   //1:房主支付  2：AA支付
--- 	required int32 playernum = 3; //玩家人数
--- 	required int32 ju_num =4;	//局数
--- 	required int32 difen = 5;      //底分
--- 	required int32 fengding = 6;  //封顶
+-- 	required int32 paytype = 2;   --1:房主支付  2：AA支付
+-- 	required int32 playernum = 3; --玩家人数
+-- 	required int32 ju_num =4;	--局数
+-- 	required int32 difen = 5;      --底分
+-- 	required int32 fengding = 6;  --封顶
 -- 	message XTHH_RULES
 -- 	{
--- 	required 	int32 piao_prize =1; //1:飘癞子没奖 2：飘癞子有奖
--- 	required 	int32 hu_laizinum = 2; //1：最多一个癞子胡牌 2：任意癞子胡牌
+-- 	required 	int32 piao_prize =1; --1:飘癞子没奖 2：飘癞子有奖
+-- 	required 	int32 hu_laizinum = 2; --1：最多一个癞子胡牌 2：任意癞子胡牌
 -- 	}
 -- 	required XTHH_RULES xthh_rules = 7;
 -- }
@@ -149,15 +161,15 @@ function Message_Def:C2L_PLAYER_MONEY_UPDATA_SYN()
     return msg:SerializeToString()
 end
 
--- //-----------------------------------------------------------------------------------------------------------------------------------
--- //大厅发送给客户端消息
--- 	L2C_PLAYER_LOGIN_ACK 								= 11001;   						//登入成功
--- 	L2C_PLAYER_BASEINFO_ACK 							= 11002;						//玩家基本信息
--- 	L2C_PLAYER_GAME_ROOM_CONFIG_ACK						= 11003;						//游戏房间基本配置
--- 	L2C_PLAYER_CREATE_ROOM_ACK 							= 11004;						//房间创建成功
--- 	L2C_PLAYER_ENTER_ROOM_ACK							= 11005;						//加入房间成功
--- 	L2C_PLAYER_MONEY_UPDATA_ACK							= 11006;						//货币更新成功
--- //-----------------------------------------------------------------------------------------------------------------------------------
+-- -------------------------------------------------------------------------------------------------------------------------------------
+-- --大厅发送给客户端消息
+-- 	L2C_PLAYER_LOGIN_ACK 								= 11001;   						--登入成功
+-- 	L2C_PLAYER_BASEINFO_ACK 							= 11002;						--玩家基本信息
+-- 	L2C_PLAYER_GAME_ROOM_CONFIG_ACK						= 11003;						--游戏房间基本配置
+-- 	L2C_PLAYER_CREATE_ROOM_ACK 							= 11004;						--房间创建成功
+-- 	L2C_PLAYER_ENTER_ROOM_ACK							= 11005;						--加入房间成功
+-- 	L2C_PLAYER_MONEY_UPDATA_ACK							= 11006;						--货币更新成功
+-- -------------------------------------------------------------------------------------------------------------------------------------
 function Message_Def:parseMsg(msg, data)
     local fields = msg._fields
     for k1,v1 in pairs(fields) do
@@ -238,19 +250,19 @@ function Message_Def:L2C_PLAYER_MONEY_UPDATA_ACK()
     return T
 end
 
--- //-----------------------------------------------------------------------------------------------------------------------------------
--- //客户端请求游戏服务器消息
--- 	C2M_PLAYER_ENTER_GAME_ROOM_SYN							= 21001;		//请求进入游戏房间
--- 	C2M_PLAYER_RECONNECT_GAME_SYN							= 21002;		//断线重连
--- 	C2M_PLAYER_SIT_DOWN_SYN									= 21003;		//请求坐下
--- 	C2M_PLAYER_READY_SYN									= 21004;		//玩家准备
--- 	C2M_PLAYER_LEAVE_ROOM_SYN								= 21005;		//玩家离开
--- 	C2M_PLAYER_DISMISS_ROOM_SYN								= 21006;		//请求房间解散
--- 	C2M_PLAYER_VOTE_SYN										= 21007;		//玩家解散投票
--- 	C2M_PLAYER_OUT_CARD_SYN									= 21008;		//玩家出牌
--- 	C2M_PLAYER_OPERATE_RESULT_SYN							= 21009;		//操作命令
--- 	C2M_PLAYER_TRUSTEE_SYN									= 21010;		//玩家托管
--- //--------------------------------------------------------------------------------------------------------------------------------------
+-- -------------------------------------------------------------------------------------------------------------------------------------
+-- --客户端请求游戏服务器消息
+-- 	C2M_PLAYER_ENTER_GAME_ROOM_SYN							= 21001;		--请求进入游戏房间
+-- 	C2M_PLAYER_RECONNECT_GAME_SYN							= 21002;		--断线重连
+-- 	C2M_PLAYER_SIT_DOWN_SYN									= 21003;		--请求坐下
+-- 	C2M_PLAYER_READY_SYN									= 21004;		--玩家准备
+-- 	C2M_PLAYER_LEAVE_ROOM_SYN								= 21005;		--玩家离开
+-- 	C2M_PLAYER_DISMISS_ROOM_SYN								= 21006;		--请求房间解散
+-- 	C2M_PLAYER_VOTE_SYN										= 21007;		--玩家解散投票
+-- 	C2M_PLAYER_OUT_CARD_SYN									= 21008;		--玩家出牌
+-- 	C2M_PLAYER_OPERATE_RESULT_SYN							= 21009;		--操作命令
+-- 	C2M_PLAYER_TRUSTEE_SYN									= 21010;		--玩家托管
+-- ----------------------------------------------------------------------------------------------------------------------------------------
 
 function Message_Def:C2M_PLAYER_ENTER_GAME_ROOM_SYN(data)
     local msg = Message_pb.MSG_C2M_PLAYER_ENTER_GAME_ROOM_SYN()
@@ -312,28 +324,28 @@ function Message_Def:C2M_PLAYER_TRUSTEE_SYN(data)
     return msg:SerializeToString()
 end
 
--- //-------------------------------------------------------------------------------------------------------------------------------------
--- //游戏服务器发送给客户端的消息
--- 	M2C_PLAYER_ENTER_GAME_ROOM_SYN							= 22001;			//请求进入游戏房间成功
--- 	M2C_PLAYER_RECONNECT_GAME_ACK							= 22002;			//重连成功
--- 	M2C_PLAYER_BASEINFO_ACK									= 22003;			//玩家的基本信息
--- 	M2C_PLAYER_ROOM_BASEINFO_ACK							= 22004;			//桌子的基本信息
--- 	M2C_PLAYER_ROOM_FREE_SCENE_ACK							= 22005;			//桌子空闲场景消息
--- 	M2C_PLAYER_ROOM_PLAYING_SCENE_ACK						= 22006;			//桌子战斗场景消息
--- 	M2C_PLAYER_STATE_UPDATA_ACK								= 22007;			//玩家状态更新
--- 	M2C_PLAYER_ROOM_STATE_UPDATA_ACK						= 22008;			//桌子状态更新
--- 	M2C_PLAYER_SIT_DOWN_ACK									= 22009;			//玩家坐下成功
--- 	M2C_PLAYER_READY_ACK									= 22010;			//玩家准备成功
--- 	M2C_PLAYER_OPER_LEAVE_ROOM_ACK							= 22011;			//玩家离开成功
--- 	M2C_PLAYER_DISMISS_ROOM_ACK								= 22012;			//玩家解散房间成功
--- 	M2C_PLAYER_VOTE_SYN										= 22013;			//玩家投票成功
--- 	M2C_PLAYER_GAME_START_ACK								= 22014;			//游戏开始
--- 	M2C_PLAYER_MONEY_UPDATA_ACK								= 22015;			//货币更新
--- 	M2C_PLAYER_OPERATE_NOTIFY_ACK							= 22016;			//操作提示
--- 	M2C_PLAYER_OPERATE_RESULT_ACK							= 21017;			//操作命令
--- 	M2C_SUB_GAME_END_ACK									= 21018;			//游戏结束
--- 	M2C_SUB_GAME_END_ALL_ACK								= 21019;			//所有游戏结束
--- //--------------------------------------------------------------------------------------------------------------------------------------
+-- ---------------------------------------------------------------------------------------------------------------------------------------
+-- --游戏服务器发送给客户端的消息
+-- 	M2C_PLAYER_ENTER_GAME_ROOM_SYN							= 22001;			--请求进入游戏房间成功
+-- 	M2C_PLAYER_RECONNECT_GAME_ACK							= 22002;			--重连成功
+-- 	M2C_PLAYER_BASEINFO_ACK									= 22003;			--玩家的基本信息
+-- 	M2C_PLAYER_ROOM_BASEINFO_ACK							= 22004;			--桌子的基本信息
+-- 	M2C_PLAYER_ROOM_FREE_SCENE_ACK							= 22005;			--桌子空闲场景消息
+-- 	M2C_PLAYER_ROOM_PLAYING_SCENE_ACK						= 22006;			--桌子战斗场景消息
+-- 	M2C_PLAYER_STATE_UPDATA_ACK								= 22007;			--玩家状态更新
+-- 	M2C_PLAYER_ROOM_STATE_UPDATA_ACK						= 22008;			--桌子状态更新
+-- 	M2C_PLAYER_SIT_DOWN_ACK									= 22009;			--玩家坐下成功
+-- 	M2C_PLAYER_READY_ACK									= 22010;			--玩家准备成功
+-- 	M2C_PLAYER_OPER_LEAVE_ROOM_ACK							= 22011;			--玩家离开成功
+-- 	M2C_PLAYER_DISMISS_ROOM_ACK								= 22012;			--玩家解散房间成功
+-- 	M2C_PLAYER_VOTE_SYN										= 22013;			--玩家投票成功
+-- 	M2C_PLAYER_GAME_START_ACK								= 22014;			--游戏开始
+-- 	M2C_PLAYER_MONEY_UPDATA_ACK								= 22015;			--货币更新
+-- 	M2C_PLAYER_OPERATE_NOTIFY_ACK							= 22016;			--操作提示
+-- 	M2C_PLAYER_OPERATE_RESULT_ACK							= 21017;			--操作命令
+-- 	M2C_SUB_GAME_END_ACK									= 21018;			--游戏结束
+-- 	M2C_SUB_GAME_END_ALL_ACK								= 21019;			--所有游戏结束
+-- ----------------------------------------------------------------------------------------------------------------------------------------
 function Message_Def:M2C_PLAYER_ENTER_GAME_ROOM_SYN()
     local msg = Message_pb.MSG_M2C_PLAYER_ENTER_GAME_ROOM_SYN()
     msg:ParseFromString(msgData)
