45744ef4f8b2b381b2b0fc8e6284281a5893978a
HelyLi <1772063751@qq.com>
2021-03-18 16:15:51 +0800

    dev

--- mj/frameworks/cocos2d-x/external/curl/include/android/curl/curlbuild.h
+++
@@ -1,5 +0,0 @@
-#ifdef __aarch64__
-#include "curlbuild-64.h"
-#else
-#include "curlbuild-32.h"
-#endif
--- mj/frameworks/cocos2d-x/external/curl/include/ios/openssl/opensslconf.h
+++
@@ -1,5 +0,0 @@
-#if defined(__LP64__) && __LP64__
-#include"opensslconf-64.h"
-#else
-#include"opensslconf-32.h"
-#endif
